e2c8082611e889554c6338b19abf4ef74e431cf0
Forge <xForgex@yahoo.com>
2025-07-28 16:16:21 -0400

    Update challonge_cog.py

--- cogs/challonge_cog.py
+++ cogs/challonge_cog.py
@@ -35,10 +35,10 @@ class ChallongeCog(commands.Cog):
         return aiohttp.BasicAuth(self.username, self.api_key)
 
     def load_json(path):
-    if not os.path.exists(path):
-        return {}
-    with open(path, "r") as f:
-        return json.load(f)
+        if not os.path.exists(path):
+            return {}
+        with open(path, "r") as f:
+            return json.load(f)
 
     def save_json(path, data):
         with open(path, "w") as f:
